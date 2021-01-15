a3c10f2d4aaf1ef1b0033c7221194d127e7b2ef1
bgibz <brendanjamesgibson@gmail.com>
2021-01-15 00:19:58 -0800

    experimenting with video options

---
+++ danPosts/video.md
@@ -0,0 +1,9 @@
+---
+user: "@Dan"
+author: "Dan"
+date: "2021-01-04"
+id: 4
+media: "/media/backgroundDan.mp4"
+---
+
+'Working hard or hardly working?' Hahahahahaha!
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/tailwind.css";
+import "../styles/video-react.css";
 //import "node_modules/video-react/dist/video-react.css"; // import css for video player
 
 function MyApp({ Component, pageProps }) {
