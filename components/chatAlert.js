f490e61f97d2bc609ddcd5fc57270b2a71e2ed92
bgibz <brendanjamesgibson@gmail.com>
2021-01-26 18:04:26 -0800

    made the alert notice show up properly on mobile

--- components/chatAlert.js
+++ components/chatAlert.js
@@ -8,7 +8,7 @@ export default function ChatAlert({ action }) {
   }
   return (
     <div className="fixed z-50 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex justify-center h-full pt-24 px-4 pb-20 items-center text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
